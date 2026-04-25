0f561fa44b73fc2335740e2aa3d3c872fe5e828a
__changbae <48839179+andyhong0122@users.noreply.github.com>
2026-04-25 02:09:46 -0700

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    """
+    Time: O(n), we only do a one pass over n
+    Space: O(1), we only keep primitives to keep track of indices
+    """
+    def trap(self, height: List[int]) -> int:
+        if not height: return 0;
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            """
+            Determine bottleneck. At the finishing point leftMax and rightMax
+            will meet. 
+            """
+            if leftMax < rightMax:
+                l += 1
+                """
+                Here leftMax is L wall, and height[l] is R wall
+                """
+                leftMax = max(leftMax, height[l])
+                """ 
+                If old L wall was higher than new R wall, then we can capture water.
+                This only works because we know there is a higher right wall somewhere.
+                """
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                """
+                rightMax is the older R wall, and height[r] becomes the new L wall.
+                """
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        
+        """
+        The condition for the while loop termination is when l meets r.
+        This means we have finished traversal using two pointers.
+        Return accumulated water.
+        """
+        return res
+        
+        
