e4c0b33a0582baba528a5121175f631201353f5a
__changbae <48839179+andyhong0122@users.noreply.github.com>
2026-04-27 14:56:16 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.js
@@ -0,0 +1,25 @@
+class Solution {
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        // Dictionaries to store char:count
+        let dictS = {}
+        let dictT = {}
+
+        // Iterate through first sample, store to dict
+        for (let i = 0; i < s.length; i++) {
+            dictS[s[i]] = (dictS[s[i]] || 0) + 1
+            dictT[t[i]] = (dictT[t[i]] || 0) + 1
+        }
+
+        for (let key in dictS) {
+            if (dictS[key] !== dictT[key]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
