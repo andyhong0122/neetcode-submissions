18b1ed67637f45bbe4cc27b07e6fc019ad6511e4
__changbae <48839179+andyhong0122@users.noreply.github.com>
2026-04-28 11:02:42 -0700

    Update: two-integer-sum - submission-1

--- Data Structures & Algorithms/two-integer-sum/submission-1.cs
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cs
@@ -1,18 +1,29 @@
+// Dictionary, sort each char
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> dict = new Dictionary<int, int>();
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        // Keep a dictionary to track string:[strings]
+        // O(n x k)
+        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
 
-        for (int i = 0; i < nums.Length; i++) 
-        {
-            int comp = target - nums[i];
+        // For each entry, create a key for its sorted characters
+        // O(n x k log k)
+        // n = number of strings
+        // k log k = sorting each chracter within string
+            // k = length of each string
+            // k log k = sorting characters
+        foreach (string str in strs) {
+            string key = new string(str.OrderBy(character => character).ToArray());
 
-            if (dict.ContainsKey(comp)) 
-            {
-                return new int[] { dict[comp], i };
+            // for sorted string, check in dict
+            if (!dict.TryGetValue(key, out List<string> list)) {
+                list = new List<string>();
+                dict[key] = list;
             }
-            dict[nums[i]] = i;
+
+            list.Add(str);
         }
 
-        return Array.Empty<int>();
+        // Return all values in the dictionary
+        return dict.Values.ToList();
     }
 }
